8eb442893b875d0bc4a15375b003fa5e27763b5f
TinyTakinTeller <tinytakinteller@gmail.com>
2024-12-19 20:40:31 +0100

    fix: align options menu ui

--- godot/scenes/component/control/control_resize_text_font/control_resize_text_font.gd
+++ godot/scenes/component/control/control_resize_text_font/control_resize_text_font.gd
@@ -5,7 +5,7 @@
 ## Attach to parent node.
 ## Adjusts font size in all children, relative to original window size.
 ## The scaling mode keeps aspect ratio (picks the smaller of the x and y resize ratios).
-## Can also resize separation and custom minimum size to make sure no-font UI is scaling properly.
+## Can also resize separation and custom minimum size (useful for keeping control nodes aligned).
 ##
 ## Resize Settings
 ## Configure minimum_width and minimum_height exports to down scale only after a certain threshold.
@@ -19,16 +19,16 @@ const DEFAULT_FONT_SIZE: int = 16
 const DEFAULT_SEPARATION: int = 0
 const DEFAULT_MINIMUM_SIZE: Vector2 = Vector2(0, 0)
 
+@export_category("Resize Font Size")
+@export var resize_font: bool = true
+@export var minimum_font_size: int = 16
+
 @export_category("Resize Separation")
 @export var resize_separation: bool = false
 
 @export_category("Resize Custom Minimum Size")
 @export var resize_minimum_size: bool = true
 @export var minimum_size_ratio_limit: float = 0.5
-
-@export_category("Resize Font Size")
-@export var resize_font: bool = true
-@export var minimum_font_size: int = 16
 
 @export_category("Resize Settings")
 @export var minimum_width: int = 0
