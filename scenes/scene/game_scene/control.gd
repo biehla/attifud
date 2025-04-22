5cab7ece7143dba2a74c4b65f6f93677ea4b6043
Anand Biehl <36610944+biehla@users.noreply.github.com>
2025-04-22 16:27:30 -0400

    added file browser

---
+++ scenes/scene/game_scene/control.gd
@@ -0,0 +1,6 @@
+class_name GameContent
+extends Control
+## Original File MIT License Copyright (c) 2024 TinyTakinTeller
+
+@onready var control_grab_focus: ControlGrabFocus = %ControlGrabFocus
+@onready var pause_menu_button: MenuButtonClass = %PauseMenuButton
