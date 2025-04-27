c26504c43b5507df61048e38bfb51ffcd115a145
Anand Biehl <36610944+biehla@users.noreply.github.com>
2025-04-27 15:21:41 -0400

    fixed i think

--- attifood/Levels/dev_test_world_1.gd
+++ attifood/Levels/dev_test_world_1.gd
@@ -1,2 +1,3 @@
-@onready var control_grab_focus: ControlGrabFocus = %ControlGrabFocus
+extends Node3D
 @onready var pause_menu_button: MenuButtonClass = %PauseMenuButton
+@onready var control_grab_focus: ControlGrabFocus = %ControlGrabFocus
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -2,11 +2,13 @@
 extends CharacterBody3D
 
 @export_category("Player")
-@export_range(1, 35, 1) var speed: float = 10  # m/s
+@export_range(1, 35, 1) var speed: float = 5 # m/s
 @export_range(10, 400, 1) var acceleration: float = 100  # m/s^2
 
 @export_range(0.1, 3.0, 0.1) var jump_height: float = 1  # m
 @export_range(0.1, 3.0, 0.1, "or_greater") var camera_sens: float = 1
+
+#@export_range()
 
 var jumping: bool = false
 var mouse_captured: bool = false
